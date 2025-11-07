3c1858b7133ea9a1d5ff2ba1f7162d8085288dc7
prakar2005 <karthik.pat2005@gmail.com>
2025-11-07 22:12:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 # -------------------------------------------------------
 # Load ML Model + Encoder
 # -------------------------------------------------------
-MODEL_PATH = '/etc/secrets/best_model (1).keras'
-ENCODER_PATH = '/etc/secrets/label_encoder (3).pkl'
+MODEL_PATH = '/etc/secrets/best_model_1.keras'
+ENCODER_PATH = '/etc/secrets/label_encoder_3.pkl'
 model, label_encoder, CLASS_NAMES = None, None, None
 IMG_SIZE = (128, 128)
 
